4f6d5ecc419b637a2e4e071df60e71c4a9a8fa84
admin <admin@admin.com>
2017-06-20 07:21:45 +0300

    Create migrations

---
+++ practice/database/migrations/2017_06_17_102613_create_feedback.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedback extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('feedback');
+    }
+}
