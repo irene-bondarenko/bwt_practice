d2a499a0a2a6c26ec7921442dfa9a7a1925ce218
admin <admin@admin.com>
2017-06-20 09:34:37 +0300

    Добавлена страница благодарности за отзыв

---
+++ practice/app/Http/Controllers/FrontController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Feedback;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class FrontController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('site.index');
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+
+
+
+
+
+    public function weather()
+    {
+        return view('site.weather');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function text()
+    {
+        return view('site.text');
+    }
+
+    public function feedback()
+    {
+
+        return view('site.feedback')
+
+
+            ;
+    }
+    public function thanks()
+    {
+
+        return view('site.thanks')
+
+
+            ;
+    }
+
+
+}
---
+++ practice/resources/views/site/thanks.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.main')
+@section('content2')
+    <div class="page-header">
+        <h1>Спасибо за отзыв!</h1>
+    </div>
+
+
+@endsection
