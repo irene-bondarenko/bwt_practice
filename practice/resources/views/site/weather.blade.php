1204e08558ffde496e727fb6e75e836a3ff6052d
admin <admin@admin.com>
2017-06-20 15:11:10 +0300

    Отображения температуры воздуха на сегодня

--- practice/resources/views/site/weather.blade.php
+++ practice/resources/views/site/weather.blade.php
@@ -15,6 +15,18 @@
 
     @if(!Auth::guest())
 
+
+
+
+        <?php
+        $text = file_get_contents( 'https://www.gismeteo.ua/weather-zaporizhia-5093/' );
+        preg_match( '/<div class="temp">(.*?)<\\/div>/is' , $text , $title );
+        echo $title[1];
+        ?>
+
+
+
+
     @endif
     @if(Auth::guest())
         <h3>Чтобы просматривать данную страницу <a href="/login">войдите</a> или <a href="/register">зарегистрируйтесь</a></h3>
